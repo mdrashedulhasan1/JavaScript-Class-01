43da6af6391ebf6b9ce61c49cbea1dd6ce05f1dc
Rashedul Hasan <Rashedul Hasan>
2021-11-08 00:47:21 +0600

    Basic JavaScript

---
+++ index.js
@@ -0,0 +1,59 @@
+//console.log("Hello World!");
+
+
+//Variable
+
+// var variableNeed = 5;
+// console.log(variableNeed);
+
+// var burgerPrice = 120;
+// console.log(typeof burgerPrice);
+
+// var myName = 'tapu';
+// console.log(typeof myName);
+
+// var isGood = true;
+// var isbad = false;
+// console.log(typeof isGood);
+// console.log(typeof isbad);
+
+
+//String
+
+// var myLove = "I love Bangladesh";
+// console.log(myLove.toLowerCase());
+// console.log(myLove.toUpperCase());
+// console.log(myLove.indexOf("e"));
+// console.log(myLove.split(" "));
+
+
+//Integer,Float,parseInt,parseFloat
+// var number1 = parseFloat("10.23456");
+// var number2 = 5;
+// console.log(number1+number2);
+
+// var number1 = .1;
+// var number2 = .2;
+// var result = number1+number2;
+// console.log(result.toFixed(2));
+
+
+// var firstName = "Rashedul";
+// var lastName = "Hasan";
+// var fullName = firstName+" "+lastName;
+// console.log(fullName);
+
+
+//Mathematical Operation
+// var number1 = 10;
+// var number2 = 3;
+// var add = number1+number2;
+// var sub = number1-number2;
+// var multi = number1*number2;
+// var div = number1/number2;
+// var remi = number1%number2;
+// console.log("Add number is:",add);
+// console.log("Sub number is:",sub);
+// console.log("Multi number is:",multi);
+// console.log("Div number is:",div);
+// console.log("Remi number is:",remi);
